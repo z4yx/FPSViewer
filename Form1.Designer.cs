92c15eeb4f53d09e373d3e42ed220d7bb83b1010
593141477 <593141477@qq.cpm>
2012-12-29 23:01:33 +0800

    first version!

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FPSViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开xml";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(522, 156);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(186, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(269, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(12, 203);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(522, 71);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.Location = new System.Drawing.Point(12, 280);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(522, 71);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsReturn = true;
+            this.textBox4.Location = new System.Drawing.Point(12, 357);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(101, 147);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.AcceptsReturn = true;
+            this.textBox5.Location = new System.Drawing.Point(119, 357);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(101, 147);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.AcceptsReturn = true;
+            this.textBox6.Location = new System.Drawing.Point(226, 357);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox6.Size = new System.Drawing.Size(308, 105);
+            this.textBox6.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 468);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 36);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "保存数据文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 468);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace FPSViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string xmlfile;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "FPS题目文件(*.xml)|*.xml";
+            op.ValidateNames = true;
+            op.CheckFileExists = true;
+            op.CheckPathExists = true;
+            op.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            if (op.ShowDialog() == DialogResult.OK) xmlfile = op.FileName;
+            else return;
+
+            XmlTextReader xmldoc = new XmlTextReader(xmlfile);
+            int i = 1;
+            try
+            {
+                while (xmldoc.Read())
+                {
+                    if(xmldoc.NodeType==XmlNodeType.Element){
+                        if (xmldoc.Name == "title")
+                        {
+                            xmldoc.Read();
+                            comboBox1.Items.Add(i.ToString() + ". " + xmldoc.Value);
+                            i++;
+                        }
+                        //textBox1.Text += Environment.NewLine + xmldoc.Name+" " ;
+                    }
+                }
+            }
+            catch (XmlException exp)
+            {
+                MessageBox.Show("文件格式错误!");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex,i=-1;
+            XmlTextReader xmldoc = new XmlTextReader(xmlfile);
+            while (xmldoc.Read())
+                if (xmldoc.NodeType == XmlNodeType.Element)
+                {
+                    Application.DoEvents();
+                    if (xmldoc.Name == "item") i++;
+                    if (i == index) {
+                        if (xmldoc.Name == "description")
+                        {
+                            xmldoc.Read(); textBox1.Text = xmldoc.Value;
+                        }
+                        else if (xmldoc.Name == "input")
+                        {
+                            xmldoc.Read(); textBox2.Text = xmldoc.Value;
+                        }
+                        else if (xmldoc.Name == "output")
+                        {
+                            xmldoc.Read(); textBox3.Text = xmldoc.Value;
+                        }
+                        else if (xmldoc.Name == "sample_input")
+                        {
+                            xmldoc.Read(); textBox4.Text = xmldoc.Value;
+                        }
+                        else if (xmldoc.Name == "sample_output")
+                        {
+                            xmldoc.Read(); textBox5.Text = xmldoc.Value;
+                        }
+                        else if (xmldoc.Name == "hint")
+                        {
+                            xmldoc.Read(); textBox6.Text = xmldoc.Value;
+                        }
+                    }
+                    else if (i > index) break;
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = "C:";
+
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK) path = dlg.SelectedPath;
+            else return;
+
+            //Char[] buf=new Char[10000000];
+            int index = comboBox1.SelectedIndex, i = -1,c=0;
+            if (index < 0) { MessageBox.Show("请先选择题目!"); return; }
+            XmlTextReader xmldoc = new XmlTextReader(xmlfile);
+
+            while (xmldoc.Read())
+                if (xmldoc.NodeType == XmlNodeType.Element)
+                {
+                    Application.DoEvents();
+                    if (xmldoc.Name == "item") i++;
+                    if (i == index)
+                    {
+                        if (xmldoc.Name == "test_input")
+                        {
+                            c++;
+                            //int len=xmldoc.ReadChars(buf,0,10000000);
+                            //StreamWriter sw = new StreamWriter(path + "\\data" + c.ToString() + ".in", false, Encoding.ASCII);
+                            xmldoc.Read();
+                            File.WriteAllText(path + "\\data" + c.ToString() + ".in", xmldoc.Value, Encoding.ASCII);
+                            
+                        }
+                        else if (xmldoc.Name == "test_output")
+                        {
+                            xmldoc.Read();
+                            File.WriteAllText(path + "\\data" + c.ToString() + ".out", xmldoc.Value, Encoding.ASCII);
+                        }
+                    }
+                    else if (i > index) break;
+                }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
